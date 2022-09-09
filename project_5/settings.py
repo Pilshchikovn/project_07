0e5d17b9ae6e282aea125d917324abbd54ee53d8
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-10 00:46:24 +0400

    changed css for main page

--- project_5/settings.py
+++ project_5/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trip'
+    'trip',
+    'tours',
+    'django_extensions',
+
 ]
 
 MIDDLEWARE = [
---
+++ tours/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('buy_tour/', views.buy_tour),
+    # path('free_tour/', views.free_tour),
+    # path('<int:each_place>/', views.get_info_about_place_by_number),  # конвертер с редиректом
+    # path('<str:each_place>/', views.get_info_about_place, name='favourite_place'),
+    # path('', views.allplaces, name='main_page'),
+]
