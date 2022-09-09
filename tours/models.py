d76e650433da1502eda883cca7fde5a43114c0b3
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-10 01:23:27 +0400

    migrations

--- tours/models.py
+++ tours/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Hotel(models.Model):
     name = models.CharField(max_length=60)
     rating = models.IntegerField()
+    year = models.IntegerField(default=2000)
+    size = models.IntegerField(null=True)
+
+
+    def __str__(self):
+        return f'{self.name} {self.rating}'
