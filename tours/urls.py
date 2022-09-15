ac804691a28488fc65a0db9e7d757e5c3641c631
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-15 08:21:35 +0400

    let's try cbv

--- tours/urls.py
+++ tours/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path('hotels/', views.allhotels, name='main_page'),
-    path('hotels/<int:id_hotel>', views.get_info_about_hotel, name='hotel-detail')
-}
+urlpatterns = [
+    path('hotels/', views.ListHotel.as_view(), name='main_page'),
+    # path('hotels/', views.allhotels, name='main_page'),
+    path('hotels/<int:pk>', views.DetailHotel.as_view(), name='hotel-detail'),
+    # path('hotels/<int:id_hotel>', views.get_info_about_hotel, name='hotel-detail')
+]
--- tours/views.py
+++ tours/views.py
@@ -2,15 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from .models import Hotel
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView,UpdateView
 
 
-def allhotels(request):
-    all = Hotel.objects.all()
-    return render(request, 'tours/hotels.html', context={'all_hotels': all})
+
+class ListHotel(ListView):
+    template_name = 'tours/hotels.html'
+    model = Hotel
+    # context_object_name = 'feedback'
+
+    # def get_queryset(self):
+    # фильтрует данные
+    #     queryset=super().get_queryset()
+    #     filter_qs = queryset.filter(rating__gt=4)
+    #     return filter_qs
+
+# def allhotels(request):
+#     all = Hotel.objects.all()
+#     return render(request, 'tours/hotels.html', context={'all_hotels': all})
 
 
-def get_info_about_hotel(request, id_hotel: int):
+
+class DetailHotel(DetailView):
+    template_name = 'tours/info_about_hotel.html'
+    model = Hotel
+    # или можно задать свое имя
+    # context_object_name = 'feed'
+
+
+# def get_info_about_hotel(request, id_hotel: int):
     # description = Hotel.objects.get(id=id_hotel)
     # благодаря get_object_or_404 можно эту строчку записать по другому:
-    description = get_object_or_404(Hotel, id=id_hotel)
-    return render(request, 'tours/info_about_hotel.html', context={'description': description})
+    # description = get_object_or_404(Hotel, id=id_hotel)
+    # return render(request, 'tours/info_about_hotel.html', context={'description': description})
